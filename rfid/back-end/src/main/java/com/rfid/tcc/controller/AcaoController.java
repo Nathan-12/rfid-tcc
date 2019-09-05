3df3cbb0f1e29f421713bd8aeb9bbdc3dffe6b8f
Nathan Lima Batista <nathanlima.b@gmail.com>
2019-09-05 10:48:40 -0300

    metodo para abrir arquivo com input e press ENTER

--- rfid/back-end/src/main/java/com/rfid/tcc/controller/AcaoController.java
+++ rfid/back-end/src/main/java/com/rfid/tcc/controller/AcaoController.java
@@ -2,17 +2,10 @@ package com.rfid.tcc.controller;
 
 import com.rfid.tcc.service.AcaoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/libras")
