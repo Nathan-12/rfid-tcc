4ffa1b4fb8acb9b54d7a1d4edfcf7a9342478487
Nathan Lima Batista <nathanlima.b@gmail.com>
2019-10-02 23:34:55 -0300

    Adicionado vuetify no projeto

---
+++ rfid/frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span class="font-weight-light">SISTEMA </span>
+        <span>RFID</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+  
+  </v-app>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  
+  data: () => ({
+    //
+  }),
+};
+</script>
